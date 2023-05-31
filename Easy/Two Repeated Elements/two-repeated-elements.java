db863a1e4120cdd29e1695f529d2f64edce7a503
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2023-05-31 13:52:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Two Repeated Elements/two-repeated-elements.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int t = Integer.parseInt(br.readLine());
+	    for(int i=0;i<t;i++){
+	        int n = Integer.parseInt(br.readLine());
+	        String l = br.readLine();
+    		String[] sarr = l.split(" ");
+    		int[] arr = new int[sarr.length];
+    		for(int i1=0;i1<arr.length;i1++){
+    			arr[i1] = Integer.parseInt(sarr[i1]);
+    		}
+    		
+            Solution obj = new Solution();
+            
+            int[] res = obj.twoRepeated(arr, n);
+            System.out.println(res[0] + " " + res[1]);
+	    }
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find two repeated elements.
+    public int[] twoRepeated(int arr[], int N)
+    {
+        int j =0;
+        int[] res = new int[N];
+        for(int i =0; i<N+2; i++)
+        {
+            int abs_val = Math.abs(arr[i]);
+            if(arr[abs_val] > 0)
+            {
+                 arr[abs_val] = -arr[abs_val];
+            }
+            else
+            {
+             res[j] = abs_val;
+             j++;
+            }
+            
+        }
+        return res;
+    }
+    
+}
