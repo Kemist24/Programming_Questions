475bc311f7b458aebbffa491194e744b0ae611f3
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2023-06-07 12:28:09 +0530

    Time: 9 ms (99.27%), Space: 42.8 MB (34.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+         if (x<0 || (x!=0 && x%10==0)) return false;
+        int res = 0;
+        while(x > res)
+        {
+           res = res*10 + x%10;
+           x = x/10;
+        }
+        if(x == res || x ==res/10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
