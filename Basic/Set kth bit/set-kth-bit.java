13870f46437306d964edc35a3094855e640b3687
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2023-08-03 22:14:40 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Set kth bit/set-kth-bit.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+           
+            Solution ob = new Solution();
+            int ans = ob.setKthBit(N,K);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int setKthBit(int N,int K){
+      
+    //   int bitmask = 1<<K;
+    //   int z = N & bitmask;
+       
+    //   if(z>0)
+    //     return N;
+    //     else
+    //       return N | (1<<K);\
+    
+    int mask=1<<(K);
+        return N |mask;
+        
+    }
+}
