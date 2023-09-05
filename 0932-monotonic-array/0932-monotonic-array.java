23c67e6c1f5c31cee861e36568db30f36729b6c7
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2023-09-05 11:07:12 +0530

    Time: 9 ms (5.09%), Space: 54.4 MB (99.73%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,21 +1,21 @@
 class Solution {
 
-    public static boolean dec(int nums[])
+    public static boolean dec(ArrayList<Integer> list)
     {
-        for(int i =0; i<nums.length-1; i++)
+        for(int i =0; i<list.size()-1; i++)
         {
-            if(nums[i]<nums[i+1])
+            if(list.get(i)<list.get(i+1))
             {
                 return false;
             }
         }
         return true;
     }
-    public static boolean inc(int nums[])
+    public static boolean inc(ArrayList<Integer> list)
     {
-        for(int i =0; i<nums.length-1; i++)
+        for(int i =0; i<list.size()-1; i++)
         {
-            if(nums[i]>nums[i+1])
+            if(list.get(i)>list.get(i+1))
             {
                 return false;
             }
@@ -26,6 +26,16 @@ class Solution {
 
 
     public boolean isMonotonic(int[] nums) {
-       return inc(nums) || dec(nums);
+       
+
+        
+
+         ArrayList<Integer> list = new ArrayList<>();
+        for (int value : nums) {
+            list.add(value);
+        }
+
+        
+       return inc(list) || dec(list);
     }
 }
