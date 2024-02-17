9e989f3555a77843fa0445c3e5ede71d0e27b73d
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2024-02-17 10:27:16 +0530

    Time: 8 ms (93.49%), Space: 43.7 MB (84.51%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static boolean isPowerOfThree(int n) {
+        if(n <=0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        if(n%3 !=0)
+        {
+            return false;
+        }
+        
+        return isPowerOfThree(n/3);
+    }
+}
