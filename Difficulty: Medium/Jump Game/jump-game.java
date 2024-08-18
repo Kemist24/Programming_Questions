691d9779a7cdabd74c5abf9e6b5292ce9da2ab10
Purvansh Rathore <95157215+Kemist24@users.noreply.github.com>
2024-08-18 15:26:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jump Game/jump-game.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S1[] = read.readLine().split(" ");
+            
+            int[] A = new int[N];
+            
+            for(int i=0; i<N; i++)
+                A[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.canReach(A,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int canReach(int[] arr, int n) {
+       int reachable = 0;
+       
+       for(int i=0; i<n; i++){
+           if(reachable<i){
+               return 0;
+           }
+           
+           reachable = Math.max(reachable, i+arr[i]);
+           
+       }
+       return 1;
+    }
+};
